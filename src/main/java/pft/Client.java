fe68f81067f6deca99362c18a94c3d5291e0df34
Ankur Bhatia <bhatia.ankur8@gmail.com>
2016-05-16 11:23:17 +0200

    Added the logic to take care of pft file

--- src/main/java/pft/Client.java
+++ src/main/java/pft/Client.java
@@ -2,9 +2,7 @@ package pft;
 
 import pft.file_operation.IFileFacade;
 import pft.file_operation.PftFileManager;
-import pft.frames.DownloadRequest;
-import pft.frames.Frame;
-import pft.frames.UploadRequest;
+import pft.frames.*;
 
 import java.io.IOException;
 import java.net.*;
@@ -38,46 +36,30 @@ public class Client {
         Framer framer = new Framer();
         Frame frame = null;
         Deframer deframer = new Deframer();
+
+
         if (command.equals("upload")) {
             //Get the Size and SHA1 of the filename and create constructor using it
-
             uploadRequest = new UploadRequest(fileName, fileSize, sha1 );
             payload = framer.frame(uploadRequest);
-            if(frame instanceof DownloadRequest) {
-                System.out.println("True" + ((DownloadRequest) frame).filename());
-
-            }
-            else {
-                System.out.println("false");
-            }
             System.out.println("UploadRequest created: " + uploadRequest.filename() + " " + uploadRequest.sha1() + " " + uploadRequest.type() + " " + uploadRequest.size() + " " + uploadRequest.identifier());
         }
 
         else {
             byte[] bytes = "00000000000000000000".getBytes();
-
             downloadRequest = new DownloadRequest(fileName,bytes );
             payload = framer.frame(downloadRequest);
             System.out.println("DownloadRequest created: " + downloadRequest.filename() + " " + downloadRequest.sha1() + " " + downloadRequest.type() + " "  + downloadRequest.identifier());
-            frame = deframer.deframe(payload);
-            if(frame instanceof DownloadRequest) {
-                System.out.println("True" + ((DownloadRequest) frame).filename());
-
-            }
-            else {
-                System.out.println("false");
-            }
         }
 
         System.out.println(payload.toString());
-        InitiateConnection();
+        InitiateConnection(this.command);
     }
 
-    public void InitiateConnection(){
+    public void InitiateConnection(String command){
         byte [] response = new byte[512];
         Frame frame = null;
         Deframer deframer = new Deframer();
-
         DatagramSocket datagramSocket = null;
         try {
             datagramSocket = new DatagramSocket();
@@ -93,10 +75,8 @@ public class Client {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-
         DatagramPacket packet = new DatagramPacket(payload, payload.length, receiverAddress, 180);
         DatagramPacket packetResponse = new DatagramPacket(response, response.length);
-
         try {
             datagramSocket.send(packet);
             datagramSocket.receive(packetResponse);
@@ -105,6 +85,18 @@ public class Client {
             frame = deframer.deframe(dataBuffer);
             System.out.println("Response from the server: " + frame.type());
 
+            if(command.equals("upload") && ((UploadResponse) frame).status() == Status.OK) {
+                //Start upload process
+
+                System.out.println("Upload Request Accepted");
+                UploadProcess();
+            }
+            else if(command.equals("download") && ((DownloadResponse) frame).status() == Status.OK) {
+                //Start Download process
+                DownloadProcess();
+                System.out.println("Download Request Accepted");
+            }
+
 
 
         } catch (IOException e) {
@@ -112,14 +104,14 @@ public class Client {
             e.printStackTrace();
         }
     }
+    public void UploadProcess(){
+        System.out.println("Starting upload...");
+
+    }
+    public void DownloadProcess() {
+
+    }
 
 
 }
-   /* ctor()
-   creTE csocket
-
-      */
-    //fucntion upload(file name)
-
-    //function download(file name)
 
--- src/main/java/pft/Server.java
+++ src/main/java/pft/Server.java
@@ -3,9 +3,13 @@ package pft;
 /**
  * Created by ankur on 15.05.2016.
  */
+import pft.file_operation.IFileFacade;
+import pft.file_operation.PftFileManager;
 import pft.frames.*;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.SocketException;
@@ -19,8 +23,8 @@ public class Server {
         Framer framer = new Framer();
 
         UploadResponse uploadResponse;
-
-        if(CheckUploadConditions() == false) {
+        DataResponse dataResponse;
+        if(CheckUploadConditions(frame) == false) {
             //Upload not possible, send a termination
             uploadResponse = new UploadResponse(0,Status.ERROR,-1);
             payload = framer.frame(uploadResponse);
@@ -36,7 +40,34 @@ public class Server {
 
     }
     public void HandleDownload() {}
-    public boolean CheckUploadConditions(){
+    public boolean CheckUploadConditions(Frame frame){
+        //Check duplicate file with diff SHA-1
+        //Check space in disk
+        //if condition fullfilled, create pft file if not created before, write sha and offset=0
+        IFileFacade fileManager;
+        fileManager = new PftFileManager(((UploadRequest) frame).filename() + "pft");
+        if(fileManager.fileExits() == false){
+            //Create a file
+            //write sha
+            //offset 0
+
+            try {
+                RandomAccessFile raf = new RandomAccessFile(((UploadRequest) frame).filename() + "pft", "rw");
+                String originalString = ((UploadRequest) frame).sha1() + " \n0 \n";
+                String updatedString = originalString.replace("\n","\r\n");
+                raf.writeBytes(updatedString);
+            }
+            catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+
+
+
         return true;
     }
     public void HandleTermination(){}
@@ -46,7 +77,10 @@ public class Server {
     public int GetIdentifier(){
         return 12;
     }
-
+    public void PrepareUpload() {
+        byte[] b;
+        //Check the status of the file in the filename.pft
+    }
     public static void main(String [] args) {
         Server server = new Server();
         Framer framer = new Framer();
@@ -75,6 +109,8 @@ public class Server {
                     response = server.HandleUpload(frame);
                     packet = new DatagramPacket(response, response.length, packet.getAddress(), packet.getPort());
                     datagramSocket.send(packet);
+                    //Start a new thread, create a new socket for the upload and send the dataRequest
+                    break;
 
                 }
                 else if(frame.type() == 1){
@@ -91,6 +127,20 @@ public class Server {
                 e.printStackTrace();
             }
         }
+
+        //The following process is to be started in a new thread
+        byte[] datareq = new byte[512];
+        DatagramPacket dataReqPacket = new DatagramPacket(buffer, buffer.length);
+        while (true){
+            try{
+
+
+            }
+            catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+
 
     }
 }
